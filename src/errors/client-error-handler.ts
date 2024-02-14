cc5520317f0a2075a372459003c2d62c77f30809
André Lagacione <lagacioneandre@gmail.com>
2024-02-13 21:55:52 -0500

    improviments

--- src/errors/client-error-handler.ts
+++ src/errors/client-error-handler.ts
@@ -4,13 +4,13 @@ import { AppError } from './AppError';
 const clientErrorHandle = (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
-            status: 'Error',
+            status: error.statusCode,
             message: error.message
         });
     }
-
-    return response.status(500).json({
-        status: 'Error',
+    const _error: any = error;
+    return response.status(_error.statusCode).json({
+        status: _error.statusCode || 500,
         message: error.message || 'API_ERRORS.INTERNAL_SERVER_ERROR'
     });
 };
