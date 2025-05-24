808d48584b5536440595d157d35845a92b9dc0f0
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-05-24 08:38:06 -0300

    change to use externa connection

--- src/utils/GetTokenData.ts
+++ src/utils/GetTokenData.ts
@@ -6,17 +6,19 @@ import { IValidationTokenData } from "./interfaces/validation-token-data.interfa
 export class GetTokenData {
     private queue: string;
     private responseQueue: string;
+    private connection: string;
 
-    constructor(queue: string, responseQueue: string) {
+    constructor(queue: string, responseQueue: string, connection: string) {
         this.queue = queue;
         this.responseQueue = responseQueue;
+        this.connection = connection;
     }
 
     async get(request: Request<any>): Promise<IValidationTokenData | null> {
         const authHeader = request.headers.authorization;
         const token = authHeader && authHeader.split(' ')[1];
         if (!token) return null;
-        const connection = new RabbitMqManageConnection('amqp://localhost');
+        const connection = new RabbitMqManageConnection(this.connection);
         const rabbitMqService = new RabbitMqMessagesProducerService(connection);
         const userValidation = await rabbitMqService.sendDataToAPI<string, IValidationTokenData>(token, this.queue, this.responseQueue);
         return userValidation;
